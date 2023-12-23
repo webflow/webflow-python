d2d85dd5744a740f407bb3dce629ef4ffc562643
dsinghvi <dsinghvi@umich.edu>
2023-12-22 21:48:18 -0500

    update README

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,11 @@
 import pytest
 
+from webflow import authorize_url, get_access_token
+from webflow.client import Webflow, AsyncWebflow
+
 # Get started with writing tests with pytest at https://docs.pytest.org
 @pytest.mark.skip(reason="Unimplemented")
 def test_client() -> None:
-    assert True == True
+    client = Webflow(access_token="")
+    client = AsyncWebflow(access_token="")
+    client.collections.create()
