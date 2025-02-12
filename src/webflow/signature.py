3b677ec6be7dafbc491faecad27a3f5143e1ec0e
kwong <keith.wong@webflow.com>
2025-02-12 12:06:40 -0500

    add docs

--- src/webflow/signature.py
+++ src/webflow/signature.py
@@ -7,6 +7,47 @@ import hashlib
 from collections.abc import Mapping
 
 def verify(headers: Mapping, body:str , secret: str):
+    """
+    Verify that the signature on the webhook message is from Webflow
+
+    Documentation:
+    https://developers.webflow.com/data/docs/working-with-webhooks#validating-request-signatures
+
+    Parameters:
+        - headers : Mapping. The request headers in a Mapping-like object
+
+        - body : str. The request body as a UTF-8 encoded string
+
+        - secret : str. The secret key generated when creating the webhook or the OAuth client secret
+    ---
+    ```
+    from fastapi import FastAPI, Request
+    # ...
+
+    @app.post('/webhookEndpoint')
+    async def webhook_endpoint_handler(
+        request: Request,
+    ):
+        # Read the request body as a utf-8 encoded string
+        body = (await request.body()).decode("utf-8")  
+    
+        # Extract the headers as Mapping-like object
+        headers = request.headers
+
+        secret = get_secret()
+        
+        verified = verify(
+            headers=request.headers, 
+            body=(await request.body()).decode("utf-8"), 
+            secret=new_webhook.secretKey)
+
+        if verified:
+            # ...process the request normally
+        else:
+            # ...handle unathenticated request
+    ```
+    """
+    
     # Normalize header format to account for different python server implementations 
     # that may or may not normalize headers already
     normalized_headers = {k.lower(): v for k, v in headers.items()}
